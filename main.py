dbb52b5a72302d01f485cca8da882119b8e37ec1
ShadityZ <kanjpatel33@gmail.com>
2022-03-12 18:15:36 +1100

    Start rework on the menu screen

--- main.py
+++ main.py
@@ -10,10 +10,17 @@ window.borderless = False
 
 class MenuButton(Button):
     def __init__(self, text='', **kwargs):
-        super().__init__(text, scale=(.25, .075), highlight_color=color.azure, **kwargs)
+        super().__init__(text, scale=(.25, .075), highlight_color=color.gray, **kwargs)
 
         for key, value in kwargs.items():
             setattr(self, key ,value)
+
+
+def playBackgroundMusic():
+    global bgmusic
+    bgmusic = Audio("background-music")
+    bgmusic.loop = True
+    bgmusic.play()
 
 
 button_spacing = .075 * 1.25
@@ -33,12 +40,13 @@ def start_game():
     menu_parent.enabled = False
     app.destroy()
     os.system("python game/game.py")
+    exit()
 
 
 main_menu.buttons = [
-    MenuButton('start', on_click=Func(start_game)),
-    MenuButton('options', on_click=Func(setattr, state_handler, 'state', 'options_menu')),
-    MenuButton('quit', on_click=Sequence(Wait(.01), Func(sys.exit))),
+    MenuButton('Start', on_click=Func(start_game)),
+    MenuButton('Options', on_click=Func(setattr, state_handler, 'state', 'options_menu')),
+    MenuButton('Quit', on_click=Sequence(Wait(.01), Func(sys.exit))),
 ]
 for i, e in enumerate(main_menu.buttons):
     e.parent = main_menu
@@ -49,22 +57,18 @@ review_text = Text(parent=options_menu, x=.275, y=.25, text='Preview text', orig
 for t in [e for e in scene.entities if isinstance(e, Text)]:
     t.original_scale = t.scale
 
-text_scale_slider = Slider(0, 2, default=1, step=.1, dynamic=True, text='Text Size:', parent=options_menu, x=-.25)
+text_scale_slider = Slider(0, 2, default=1, step=.1, dynamic=True, text='Text Size:', 
+                           parent=options_menu, x=-.25)
 def set_text_scale():
     for t in [e for e in scene.entities if isinstance(e, Text) and hasattr(e, 'original_scale')]:
         t.scale = t.original_scale * text_scale_slider.value
 text_scale_slider.on_value_changed = set_text_scale
 
 
+options_back = MenuButton(parent=options_menu, text='Back', x=-.25, origin_x=-.5, 
+                          on_click=Func(setattr, state_handler, 'state', 'main_menu'))
 
-volume_slider = Slider(0, 1, default=Audio.volume_multiplier, step=.1, text='Master Volume:', parent=options_menu, x=-.25)
-def set_volume_multiplier():
-    Audio.volume_multiplier = volume_slider.value
-volume_slider.on_value_changed = set_volume_multiplier
-
-options_back = MenuButton(parent=options_menu, text='Back', x=-.25, origin_x=-.5, on_click=Func(setattr, state_handler, 'state', 'main_menu'))
-
-for i, e in enumerate((text_scale_slider, volume_slider, options_back)):
+for i, e in enumerate((text_scale_slider, options_back)):
     e.y = -i * button_spacing
 
 
@@ -73,10 +77,12 @@ for menu in (main_menu, options_menu):
         for i, e in enumerate(menu.children):
             e.original_x = e.x
             e.x += .1
-            e.animate_x(e.original_x, delay=i*.05, duration=.1, curve=curve.out_quad) # type: ignore
+            e.animate_x(e.original_x, delay=i*.05, duration=.1, 
+                        curve=curve.out_quad) # type: ignore
 
             e.alpha = 0
-            e.animate('alpha', .7, delay=i*.05, duration=.1, curve=curve.out_quad) # type: ignore
+            e.animate('alpha', .7, delay=i*.05, duration=.1, 
+                      curve=curve.out_quad) # type: ignore
 
             if hasattr(e, 'text_entity'):
                 e.text_entity.alpha = 0
@@ -85,7 +91,9 @@ for menu in (main_menu, options_menu):
     menu.on_enable = animate_in_menu
 
 
-background = Entity(model='quad', texture='shore', parent=camera.ui, scale=(camera.aspect_ratio,1), color=color.white, z=1)
+background = Entity(model='quad', texture='background', parent=camera.ui, 
+                    scale=(camera.aspect_ratio,1), color=color.white, z=1)
 
 
+playBackgroundMusic()
 app.run()
