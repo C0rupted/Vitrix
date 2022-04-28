de508bdac58bb38147fb58d6593e0d1818b831d1
David Patrik <davecarrijodev@gmail.com>
2022-04-28 08:03:07 +0100

    add the batle axe

---
+++ vitrix/lib/weapons/batleaxe.py
@@ -0,0 +1,16 @@
+import os
+from ursina import *
+from lib.paths import GamePaths
+
+class Batle_axe(Entity):
+    def __init__(self):
+        super().__init__(
+            parent=camera.ui,
+            position = Vec2(0.8, -0.3),
+            scale=0.1,
+            rotation=Vec3(-20, -20, -10),
+            model=os.path.join(GamePaths.models_dir, "batle_axe.obj"),
+            #texture=os.path.join(GamePaths.textures_dir, "axe.png"),
+            color=color.color(0, 0, 0.4),
+            on_cooldown=False
+        )
