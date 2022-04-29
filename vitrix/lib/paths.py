27663a367f949f54d7d47cabb4f165b751c90148
Chateauvisionn <chateauvision@outlook.fr>
2022-04-29 19:15:32 +0200

    added settings.py

--- vitrix/lib/paths.py
+++ vitrix/lib/paths.py
@@ -4,4 +4,3 @@ class GamePaths():
     models_dir = os.path.join("assets", "models")
     textures_dir = os.path.join("assets", "textures")
     sounds_dir = os.path.join("assets", "sounds")
-    
--- vitrix/lib/settings.py
+++ vitrix/lib/settings.py
@@ -1,5 +1,11 @@
 import json
 
-settings_file = open("../user/settings.json", "r").read()
+settings_file = open("vitrix/user/settings.json", "r").read()
 
 settings = json.load(settings_file)
+
+def sread():
+    pass
+
+def swrite():
+    pass
